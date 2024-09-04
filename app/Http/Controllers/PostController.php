5d07f3dd0674097edd0471caed481357f135099f
wataame89 <wataame200189@yahoo.co.jp>
2024-09-04 17:54:22 +0900

    ブログ投稿詳細の表示

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,9 +10,20 @@ class PostController extends Controller
     public function index(Post $post)
     {
         // $test = $post->orderBy('updated_at', 'DESC')->limit(2)->toSql(); //確認用に追加
-        // dd($test); //確認用に追加
+        // dd($post); //確認用に追加
 
         return view('posts.index')->with(['posts' => $post->getPaginateByLimit(2)]);
+    }
+
+    //  特定IDのpostを表示する
+    //  @params Object Post // 引数の$postはid=1のPostインスタンス
+    //  @return Reposnse post view
+
+    public function show(Post $post)
+    {
+        // dd($post);
+        return view('posts.show')->with(['post' => $post]);
+        //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
     }
 }
 ?>
