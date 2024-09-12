3d8e28475bc91c18cba22d21f97dec0704a5a261
wataame89 <wataame200189@yahoo.co.jp>
2024-09-13 00:06:08 +0900

    認証機能:Breeze

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,11 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <title>Blog</title>
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    </head>
+<x-app-layout>
+    <x-slot name="header">
+        Blog
+    </x-slot>
     <body>
         <h1>Blog Name</h1>
         <a href='/posts/create'>create</a>
@@ -38,4 +34,4 @@
             }
         </script>
     </body>
-</html>
+</x-app-layout>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,11 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <title>Blog</title>
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    </head>
+<x-app-layout>
+    <x-slot name="header">
+        Blog
+    </x-slot>
     <body>
         <h1>Blog Name</h1>
         <a href='/posts/create'>create</a>
@@ -28,6 +24,9 @@
         <div class='paginate'>
             {{ $posts->links() }}
         </div>
+        <div class='loginName'>
+            ログインユーザー:{{ Auth::user()->name }}
+        <div>
         <script>
             function deletePost(id) {
                 'use strict'
@@ -38,4 +37,4 @@
             }
         </script>
     </body>
-</html>
+</x-app-layout>
